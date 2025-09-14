7a85de81010cd921ce2c32bc50b8cb853ee6799c
mistful <99610491+mistful-stolen@users.noreply.github.com>
2025-09-14 11:37:33 +0200

    Practice string interpolation

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+string name = "Jelly";
+int age = 25;
+decimal height = 176.325m;
+
+
+Console.WriteLine($"My name is {name}; I'm {age} years young and only {height} centimeters tall.");
+Console.WriteLine($@"Name: {name}
+    Age: {age}
+    Height: {height}");
+
+    
